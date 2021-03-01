7f63cdd024a2d8dab1013b501342de4d09b5f94e
김태국 <ktg2926@douzone.com>
2021-03-01 16:00:57 +0900

    Initial Settings

---
+++ src/main/java/coding/clone/kakaoTalk/KakaoTalkCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package coding.clone.kakaoTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTalkCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTalkCloneCodingApplication.class, args);
+	}
+
+}
